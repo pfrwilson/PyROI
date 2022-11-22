3b0971b14ff20b782fcd26b4389eebf2be6c62d8
Paul Wilson <pfrwilson@gmail.com>
2022-11-22 00:12:09 -0500

    refactor

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from roi_boy.segmentation_server import SegmentationServer
-from roi_boy.backend.backend_example import DummySegmentations
+from pyroi.segmentation_server import SegmentationServer
+from pyroi.backend.backend_example import DummySegmentations
 
 app = SegmentationServer(DummySegmentations("example")).app
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
 setuptools.setup(
-    name="roi_boy",
+    name="pyroi",
     install_requires=["numpy", "matplotlib", "scikit-image", "uvicorn", "fastapi"],
-    packages=["roi_boy", "roi_boy.backend"],
-    package_data={"roi_boy": ["templates/*", "static/*"]},
+    packages=["pyroi", "pyroi.backend"],
+    package_data={"pyroi": ["templates/*", "static/*"]},
 )
